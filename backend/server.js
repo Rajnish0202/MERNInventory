1374685537aa6b690097457fbd329f01b11b2dc3
Rajnish0202 <rajnish.0202kumar@gmail.com>
2023-01-23 12:04:24 +0530

    product and contact api completed

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoute = require('./routes/userRoute');
 const productRoute = require('./routes/productRoute');
+const contactRoute = require('./routes/contactRoute');
 const errorHandler = require('./middlewares/errorMiddleware');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 
@@ -16,10 +18,12 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes Middleware
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/contactus', contactRoute);
 
 // Routes
 app.get('/', (req, res) => {
